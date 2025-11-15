5b04bf104588a0ebec4cfb6a377bcbc279bad13f
chancewalter0907 <chancewalter@rocketmail.com>
2025-11-14 23:41:56 -0600

    Canvas system foundation set up

---
+++ DrawOrDraw/Scripts/Canvas/DrawingCanvas.cs
@@ -0,0 +1,81 @@
+using System.Diagnostics;
+using Godot;
+using Steamworks;
+
+public partial class DrawingCanvas : Sprite2D
+{   
+    [Export] private string spriteName;
+    private SpriteArray2D canvas;
+    private bool drawing = false;
+    private Area2D[][] pixels;
+    [Export] private float pixelSize = 1f;
+    [Export] private int size = 32;
+
+    public override void _Ready()
+    {
+        canvas = new(size);
+        pixels = new Area2D[size][];
+        for (int i = 0; i < size; i++)
+        {
+            pixels[i] = new Area2D[size];
+            for (int j = 0; j < size; j++)
+            {
+                int a = i;
+                int b = j;
+                pixels[i][j] = new Area2D();
+                var collisionShape = new CollisionShape2D();
+                var shape = new RectangleShape2D();
+                shape.Size = new(pixelSize, pixelSize);
+                collisionShape.Shape = shape;
+
+                CallDeferred("add_child", pixels[i][j]);
+                pixels[i][j].Position = new(i * pixelSize - (size * 0.5f * pixelSize), j * pixelSize - (size * 0.5f * pixelSize));
+                pixels[i][j].CallDeferred("add_child", collisionShape);
+                pixels[i][j].MouseEntered += () => MouseEntered(b, a);
+                pixels[i][j].MouseExited += () => MouseLeft(b, a);
+            }
+        }
+    }
+    private void MouseEntered(int i, int j)
+    {
+        GD.Print($"Drawing at {i},{j}");
+
+        if (drawing)
+        {
+            canvas.Pixels[i][j].r = 0;
+            canvas.Pixels[i][j].g = 0;
+            canvas.Pixels[i][j].b = 0;
+            var img = canvas.CreateImage();
+            var texture = ImageTexture.CreateFromImage(img);
+            Texture = texture;
+        }
+    }
+    private void MouseLeft(int i, int j)
+    {
+
+    }
+
+    public override void _Process(double delta)
+    {
+        
+    }
+    public override void _Input(InputEvent e)
+    {
+        if (e is InputEventMouseMotion m)
+        {
+            
+        } 
+        else if (e is InputEventMouseButton b)
+        {
+            if (b.IsActionPressed("PrimaryAction"))
+            {
+                GD.Print("Drawing");
+                drawing = true;
+            } 
+            else if (b.IsActionReleased("PrimaryAction"))
+            {
+                drawing = false;
+            }
+        }
+    }
+}
